c6c626533aa4160c04f0521a0ae68c490ef311c5
ishitabhadra <114369699+ishitabhadra@users.noreply.github.com>
2023-08-05 13:43:05 -0700

    Update reverseString.py

--- reverseString.py
+++ reverseString.py
@@ -11,8 +11,7 @@ word = ''.join(list)
 print("Reversed string is",word)
 
 #To check if the word is a palindrome
-
-# if string == word:
-#   print("yes it is a palindrome")
-# else:
-#   print("no, not a palindrome")
+if string == word:
+  print("yes it is a palindrome")
+else:
+  print("no, not a palindrome")
